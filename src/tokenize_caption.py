c55088f294f6ae43388bb579e80d9eae21e025d5
hungcute0523-ai <hungcute0523@gmail.com>
2025-08-25 22:45:04 +0700

    Delete src/tokenize_caption.py

--- src/tokenize_caption.py
+++
@@ -1,7 +0,0 @@
-def tokenize_caption(caption: str, lowercase: bool = True):
-    TOKEN_RE = re.compile(r"[A-Za-z0-9]+(?:'[A-Za-z0-9]+)?|[^\w\s]", re.UNICODE)
-
-    if lowercase:
-        caption = caption.lower()
-    tokens = TOKEN_RE.findall(caption)
-    return tokens
