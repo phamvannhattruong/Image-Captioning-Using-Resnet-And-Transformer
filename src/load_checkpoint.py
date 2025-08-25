d8ec78a7121f1a972fe96fcff6b8a7da15d2a8cd
hungcute0523-ai <hungcute0523@gmail.com>
2025-08-25 22:44:39 +0700

    Delete src/load_checkpoint.py

--- src/load_checkpoint.py
+++
@@ -1,9 +0,0 @@
-def load_checkpoint(model, optimizer, path="checkpoint.pth"):
-
-    checkpoint = torch.load(path, map_location="cpu")
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    epoch = checkpoint["epoch"]
-    loss = checkpoint["loss"]
-    print(f" Load checkpoint từ epoch {epoch}, loss={loss}")
-    return model, optimizer, epoch, loss
