8b013ba8c8f0b1b365fd60a349e581cc559d36fa
Aphelios28 <truongphamnhat2004@gmail.com>
2025-09-05 13:01:41 +0700

    remove encode

--- src/llm/model/transformer.py
+++ src/llm/model/transformer.py
@@ -2,26 +2,16 @@ import torch
 from torch import nn
 
 from src.llm.model.decode import Decoder
-from src.llm.model.encode import Encoder
 
 
 class Transformer(nn.Module):
 
-    def __init__(self, src_pad_idx, trg_pad_idx, trg_sos_idx, enc_voc_size, dec_voc_size, d_model, n_head, max_len,
+    def __init__(self, trg_pad_idx, trg_sos_idx, dec_voc_size, d_model, n_head, max_len,
                  ffn_hidden, n_layers, drop_prob, device):
         super().__init__()
-        self.src_pad_idx = src_pad_idx
         self.trg_pad_idx = trg_pad_idx
         self.trg_sos_idx = trg_sos_idx
         self.device = device
-        self.encoder = Encoder(d_model=d_model,
-                               n_head=n_head,
-                               max_len=max_len,
-                               ffn_hidden=ffn_hidden,
-                               enc_voc_size=enc_voc_size,
-                               drop_prob=drop_prob,
-                               n_layers=n_layers,
-                               device=device)
 
         self.decoder = Decoder(d_model=d_model,
                                n_head=n_head,
