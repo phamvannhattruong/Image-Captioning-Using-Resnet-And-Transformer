1963ec2ff86bae8b9c3cf554d87dd720c3e20a4f
Aphelios28 <truongphamnhat2004@gmail.com>
2025-10-13 13:46:59 +0700

    delete file unnecessary

--- src/llm/embedding/token_embedding.py
+++
@@ -1,6 +0,0 @@
-import torch
-from torch import nn
-
-class ToKenEmbedding(nn.Embedding):
-    def __init__(self, vocab_size, dimension):
-        super (ToKenEmbedding, self).__init__(vocab_size, dimension, padding_idx=1)
