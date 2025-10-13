568bf0fc52dda5c1db047d9674e6130dc4cf573f
Aphelios28 <truongphamnhat2004@gmail.com>
2025-10-13 13:48:43 +0700

    update file config

--- config.py
+++ config.py
@@ -1,28 +1,23 @@
+import os 
 import torch
-
-#directory to folder dataset
-root_dir = "dataset/flickr30k_images/flickr30k_images"
-json_dir = "dataset/configure/captions.json"
-
-# GPU device setting
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-# model parameter setting
-batch_size = 128
-max_len = 256
-d_model = 512
-n_layers = 6
-n_heads = 8
-ffn_hidden = 2048
-drop_prob = 0.1
-
-# optimizer parameter setting
-init_lr = 1e-5
-factor = 0.9
-adam_eps = 5e-9
-patience = 10
-warmup = 100
-epoch = 1000
-clip = 1.0
-weight_decay = 5e-4
-inf = float('inf')
+#dataset paths
+data_set_root = "dataset/flickr8k"
+images_dir = f"{data_set_root}/Images"
+captions_dir = f"{data_set_root}/captions.txt"
+#train set and val set paths
+train_image_path = os.path.join(data_set_root, images_dir)
+val_image_path = os.path.join(data_set_root, images_dir)
+train_captions_file = os.path.join(data_set_root, captions_dir)
+val_captions_file = os.path.join(data_set_root, captions_dir)
+#hyper parameters
+learning_rate = 1e-4
+image_size = 128
+nepochs = 200
+batch_size = 16
+#device 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+#num of layers, heads, hidden size, patch size
+hidden_size = 192
+num_layers = (6, 6)
+num_heads = 8
+patch_size = 8
